d642b2c915386ad386f3cbfb353b12aac38ee3dc
Jose Ignacio Martin <jimartinfdez@outlook.com>
2024-04-14 13:45:31 +0200

    Updated NeoVim config and plugins

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,2 @@
+vim.g.mapleader = " "  -- Make sure to set `mapleader` before lazy so your mappings
+vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
---
+++ .config/nvim/lua/config/settings.lua
@@ -0,0 +1,14 @@
+local opt = vim.opt
+
+opt.cursorline = true
+opt.number = true
+opt.relativenumber = true
+opt.termguicolors = true
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
